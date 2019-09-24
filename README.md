bfd4dd645c4a3daca1854cbcb8bf2485c7c54c8d
Nick <nicholastysh@gmail.com>
2019-09-23 21:36:50 -0400

    updated seed, updated readme

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
-# to install:
+# install:
 
 `git clone repo`<br>
 `cd into new folder`<br>
 `npm install` (installs express dependencies)<br>
 `npm run client-install` (installs react dependencies)
 
+# populate database:
+located in `client/src/db/dbseed.sql`
+
 # run as dev:
 
 `npm run dev`
 
+
+
+# what is available so far:
+<a href="https://localhost:3000/">https://localhost:3000/</a><br>
+<a href="https://localhost:3000/login">https://localhost:3000/login</a><br>
+<a href="https://localhost:3000/register">https://localhost:3000/register</a>
 
--- client/src/db/dbseed.sql
+++ client/src/db/dbseed.sql
@@ -4,19 +4,19 @@ USE PARKP2P;
 CREATE TABLE driveways
 (
 
-    ID INT
-    AUTO_INCREMENT NOT NULL,
-	First_Name VARCHAR(100) NOT NULL,
-	Last_Name VARCHAR(100) NOT NULL,
+	ID INT
+	AUTO_INCREMENT NOT NULL,
+    Email VARCHAR(80) NOT NULL,
+    Pass VARCHAR(15) NOT NULL,
     Username VARCHAR(15) NOT NULL,
-	Phone_Number VARCHAR(15) NOT NULL,
-	Email VARCHAR(80) NOT NULL,
-	Pass VARCHAR(15) NOT NULL,
-	Address VARCHAR(100) NOT NULL,
+	First_Name VARCHAR(100),
+	Last_Name VARCHAR(100),
+	Phone_Number VARCHAR(15),
+	Address VARCHAR(100),
     Address_Extra VARCHAR(100),
-	City VARCHAR(100) NOT NULL,
-	State VARCHAR(100) NOT NULL,
-	Zip INTEGER(10) NOT NULL, 
+	City VARCHAR(100),
+	State VARCHAR(100),
+	Zip INTEGER(10), 
 	Spots INTEGER(10),
 	Active_State BOOLEAN,
 	Hourly INTEGER(10),
@@ -28,12 +28,8 @@ CREATE TABLE driveways
 	PRIMARY KEY(ID)
 );
 
-    USE PARKP2P;
-    INSERT INTO driveways
-        (First_Name, Last_Name, Username, Phone_Number, Email, Pass, Address, Address_Extra, City, State, Zip, Spots, Active_State,
-         Hourly, Daily, Weekly, Monthly, Overnight, Balance)
-    values
-        ("Nick", "Tysh", "NT5K", "2163211234", "nicholastysh@gmail.com", "hashedpassword", "1480 Felton Rd", 
-        null, "South Euclid", "OHIO", "44121", "1", true, 2, 12, null, null, 10, 100)
-	
-		
+	USE PARKP2P;
+	INSERT INTO driveways
+		(Email, Pass, Username, First_Name, Last_Name, Phone_Number, Address, Address_Extra, City, State, Zip, Spots, Active_State, Hourly, Daily, Weekly, Monthly, Overnight, Balance)
+	values
+		("Nicholastysh@gmail.com", "hashedpassword", "NT5K", null, null, null, null, null, null, null, null, null, false, null, null, null, null, null, null)
