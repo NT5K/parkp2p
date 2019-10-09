fcdb0119b049d1bca5915f9f712c5ab41718eaa5
Nick <nicholastysh@gmail.com>
2019-10-08 21:56:32 -0400

    remove login function

--- client/src/components/Dashboard_Nav_Side.js
+++ client/src/components/Dashboard_Nav_Side.js
@@ -1,49 +1,5 @@
 import React, { Component } from 'react';
 
-
-
-    logout() {
-        this.setState({
-            isLoading: true,
-        });
-        const obj = getFromStorage('park_p2p');
-        // console.log("~~~~~~~~~" , obj.token)
-        if (obj && obj.token) {
-            const { token } = obj;
-            console.log("TOKEN", token)
-            this.setState({ error: "good1" })
-
-            // Delete token from database, clear local storage
-            fetch('/api/account/logout/' + token, {
-                method: 'DELETE',
-                body: JSON.stringify(token)
-            })
-                .then(res => res.json())
-                .then(json => {
-                    if (json.success) {
-                        store.remove('park_p2p')
-                        this.setState({
-                            token: '',
-                            isLoading: false,
-                            error: "successful logout",
-                            loggedOut: true
-                        });
-
-                    } else {
-                        this.setState({
-                            isLoading: false,
-                            error: "failed to logout"
-                        });
-                    }
-                });
-        } else {
-            this.setState({
-                isLoading: false,
-            });
-        }
-    }
-
-    
 class DashboardNavSide extends Component {
     render() {
         return (
