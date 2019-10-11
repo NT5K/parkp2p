eae17a5c2e6297efd6118a18da1b7b73b8baafb6
Travis Abraham <abe.travis@outlook.com>
2019-10-10 19:51:48 -0700

    AUTOCOMPLETE SEARCH WORKS!

--- client/src/components/GoogleMap.js
+++ client/src/components/GoogleMap.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, /*InfoWindow,*/ Marker } from 'google-maps-react';
 import ExampleData from './ExampleData';
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng,
+  } from 'react-places-autocomplete';
 require('dotenv').config()
 
 const styles = require('./GoogleMapStyles.json')
@@ -17,13 +21,15 @@ class MapContainer extends Component {
             activeMarker: {},          //Shows the active marker upon click
             selectedPlace: {},
             Address: '',
+            address: '',
+            lat1: 0,
+            Lng1: 0,
             lat: 0,
             lng: 0,
             marker: []
         };
-        
         this.onMarkerClick = this.onMarkerClick.bind(this);
-        
+        this.handleSelect = this.handleSelect.bind(this);
     }
 
     UNSAFE_componentWillMount() {
@@ -49,11 +55,62 @@ class MapContainer extends Component {
         }
       };
 
+      handleChange = address => {
+        this.setState({ address });
+      };
+     
+      handleSelect = address => {
+        geocodeByAddress(address)
+          .then(results => getLatLng(results[0]))
+          .then(results => this.setState({lat1: results.lat, Lng1: results.lng}))
+        // .then(() => console.log(this.state.lat1))
+          .catch(error => console.error('Error', error));
+          
+      };
+
     render() {
-        const { marker } = this.state
+        const { marker, lat1, Lng1 } = this.state
         return (
             <div>
              <div style={{ position: "relative", width: "100vw", height: "50vh" }} className="">
+             <PlacesAutocomplete
+        value={this.state.address}
+        onChange={this.handleChange}
+        onSelect={this.handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Places ...',
+                className: 'location-search-input',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
          <Map
                     google={this.props.google}
                     zoom={12}
@@ -61,6 +118,7 @@ class MapContainer extends Component {
                     // initialCenter={{
                     //     lat: 41.50416, lng: -81.60845
                     // }}
+                    center={{lat: lat1, lng: Lng1}}
                     centerAroundCurrentLocation={true}
                     fullscreenControl={false}
                     streetViewControl={false}
