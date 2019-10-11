28153e1f8d5c4a6471889d8787060cb3381cf80c
Level126Noob <38739330+Level126Noob@users.noreply.github.com>
2019-10-11 00:19:50 -0400

    fixed bugs

--- routes/connection.js
+++ routes/connection.js
@@ -7,11 +7,11 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
     connection = mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,  // your password
-        database: process.env.DB_DATABASE,
-        port: process.env.DB_PORT
+        host: 'v02yrnuhptcod7dk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 	"aozs6jzl699tlqgd",
+        password: "w5p4hrzpzrvcs182",  // your password
+        database: "oi7dfptdy8jfy4dq",
+        port: 	3306
     });
 };
 
