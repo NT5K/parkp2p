742799a362cbe2971baf6eaa26d118a7961a2a48
Nick <nicholastysh@gmail.com>
2019-10-13 11:21:48 -0400

    var to let

--- routes/connection.js
+++ routes/connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 require('dotenv').config()
 
-const connection;
+let connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL)
